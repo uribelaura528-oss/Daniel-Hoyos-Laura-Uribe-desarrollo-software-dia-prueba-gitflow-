e2c8e2a92a2c047ba31987d92fff61c7ebe44044
lur <lur5856@gmail.com>
2025-09-05 11:09:38 -0500

    Laura Uribe PROGRAMADORA A primer commit

---
+++ src/main/java/com/mycompany/parcial/Inventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcial;
+
+/**
+ *
+ * @author Laura
+ */
+public class Inventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
